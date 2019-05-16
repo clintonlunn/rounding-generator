0bf105a9e39d219b39ce5d36a5078034c548446a
Clinton Lunn <cpl09c@gmail.com>
2019-05-16 06:50:04 -0400

    add validation classes

--- app.js
+++ app.js
@@ -7,15 +7,44 @@ const resultsValue = document.getElementById("result");
 const calculateBtn = document.getElementById("calculate");
 const resetBtn = document.getElementById("reset");
 
+function checkValidity() {
+
+}
+
 // Event listener logic for calculate button or enter key
 calculateBtn.addEventListener("click", function (e) {
     // execute rounding function
-    if (inputValue.value !== "" && inputValue.value > 0) {
+    if (inputValue.value !== "" && inputValue.value > 0 && roundingValue.value !== "" && roundingValue.value > 0) {
         roundingCalc(inputValue.value, roundingValue.value);
+
+        inputValue.classList.remove("is-invalid");
+        inputValue.classList.add("is-valid");
+
+        roundingValue.classList.remove("is-invalid");
+        roundingValue.classList.add("is-valid");
+
+
+
+    } else {
+        if (inputValue.value === "" || inputValue < 0) {
+            // add invalid class to inputValue
+            inputValue.classList.remove("is-valid");
+            inputValue.classList.add("is-invalid");
+        }
+
+        if (roundingValue.value === "" || roundingValue.value < 0) {
+            // add invalid class to roundingValue
+            roundingValue.classList.remove("is-valid");
+            roundingValue.classList.add("is-invalid");
+        }
     }
 });
 
 // Event listener logic for enter key within input MAYBE
+inputValue.addEventListener("submit", function (e) {
+
+});
+
 // Event listener logic for reset button
 resetBtn.addEventListener("click", function (e) {
     roundingValue.value = "";
@@ -28,27 +57,13 @@ function roundingCalc(inputVal, roundingVal) {
     const baseNum = inputVal - remainder;
     roundingVal = parseInt(roundingVal);
 
-    console.log({
-        baseNum,
-        remainder
-    });
-
     if ((remainder >= (roundingVal / 2))) {
         // round up
-        console.log('rounding up');
-
-        console.log({
-            baseNum: typeof baseNum,
-            roundingVal: typeof roundingVal
-        });
-        const result = baseNum + roundingVal;
-        console.log(result);
-        resultsValue.innerText = (baseNum + roundingVal).toString();
+        resultsValue.value = baseNum + roundingVal
 
     } else {
         // round down
-        console.log('rounding down');
-        resultsValue.innerText = baseNum.toString();
+        resultsValue.value = baseNum
     }
 
 
